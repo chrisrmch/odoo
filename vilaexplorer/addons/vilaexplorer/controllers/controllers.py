dd282980443c44ca8200b57e880530940bbcc3ed
Ranger <aangelcruzmx@gmail.com>
2024-10-17 11:20:49 +0200

    Initial commit

---
+++ vilaexplorer/addons/vilaexplorer/controllers/controllers.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# from odoo import http
+
+
+# class Vilaexplorer(http.Controller):
+#     @http.route('/vilaexplorer/vilaexplorer', auth='public')
+#     def index(self, **kw):
+#         return "Hello, world"
+
+#     @http.route('/vilaexplorer/vilaexplorer/objects', auth='public')
+#     def list(self, **kw):
+#         return http.request.render('vilaexplorer.listing', {
+#             'root': '/vilaexplorer/vilaexplorer',
+#             'objects': http.request.env['vilaexplorer.vilaexplorer'].search([]),
+#         })
+
+#     @http.route('/vilaexplorer/vilaexplorer/objects/<model("vilaexplorer.vilaexplorer"):obj>', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('vilaexplorer.object', {
+#             'object': obj
+#         })
